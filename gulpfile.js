f699af13dd55c4ab1af048a8daa4e12cd2f3c47a
Stas <melnik909@ya.ru>
2016-03-15 11:48:23 +0300

    добавил переменную distFolder

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,19 @@ watch = require("gulp-watch"),
 fileInclude = require("gulp-file-include"),
 htmlMin = require("gulp-html-minifier"),
 
-svgMin = require("gulp-svgMin");
+svgMin = require("gulp-svgMin"),
 
-
-
-gulp.task("html", function(){
-
-	gulp.src("index.html").pipe(htmlMin({collapseWhitespace: true})).pipe(gulp.dest("dist"));
-
-});
+distFolder = "dist";
 
 gulp.task("build-files", function(){
     
-    gulp.src("html/pages/*.html").pipe(fileInclude({prefix: "@@",basepath: "@file"})).pipe(htmlMin({collapseWhitespace: true})).pipe(gulp.dest("dist"));
+    gulp.src("html/pages/*.html").pipe(fileInclude({prefix: "@@",basepath: "@file"})).pipe(htmlMin({collapseWhitespace: true})).pipe(gulp.dest(distFolder));
 	
 });
 
 gulp.task("svg", function(){
 	
-	gulp.src("icons/*.svg").pipe(svgMin()).pipe(gulp.dest("dist/icons"));
+	gulp.src("icons/*.svg").pipe(svgMin()).pipe(gulp.dest(distFolder + "/icons"));
 	
 });
 
